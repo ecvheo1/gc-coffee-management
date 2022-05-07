e726790287b023b173a92d9e5d42c717ef90f043
ecvheo1 <ecvheo@naver.com>
2022-05-08 02:32:47 +0900

    :white_check_mark: order 생성 service 테스트 코드 구현

---
+++ gc-coffee-management/src/test/java/com/example/gccoffeemanagement/OrderServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.gccoffeemanagement;
+
+import com.example.gccoffeemanagement.domain.Order;
+import com.example.gccoffeemanagement.dto.OrderCreateRequest;
+import com.example.gccoffeemanagement.dto.OrderProductCreateRequest;
+import com.example.gccoffeemanagement.repository.OrderRepository;
+import com.example.gccoffeemanagement.service.OrderService;
+import com.example.gccoffeemanagement.service.OrderServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    OrderService orderService;
+
+    @Mock
+    OrderRepository orderRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        MockitoAnnotations.openMocks(this);
+        orderService = new OrderServiceImpl(orderRepository);
+    }
+
+    @Test
+    void order_저장_요청이_들어올_때_전달받은_order가_null이면_IllegalStateException이_발생한다() {
+        //given
+        final Order order = null;
+
+        //when, then
+        assertThrows(IllegalStateException.class, () -> orderService.saveOrder(order));
+    }
+
+    @Test
+    void order_저장_요청이_들어올_때_정상적인_요청_데이터라면_orderRepository의_save_메소드를_호출한다() {
+        //given
+        final Order order = newOrder();
+
+        //when
+        orderService.saveOrder(order);
+
+        //then
+        verify(orderRepository, times(1)).save(any(Order.class));
+    }
+
+    private Order newOrder() {
+        return Order.of(
+                new OrderCreateRequest(
+                        "test@test.com",
+                        "test address",
+                        "11111",
+                        List.of(new OrderProductCreateRequest(1, 1))
+        ));
+    }
+}
